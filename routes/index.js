f70fead63b26b599cd9bba6881ad424a07249c34
wmx <waminxi@163.com>
2018-04-27 18:44:40 +0800

    study something

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // 第一个参数是模板的名称，即views目录下的文件名；第二个参数是传递给模板的数据
   res.render('index', { title: 'Express' });
 });
 
